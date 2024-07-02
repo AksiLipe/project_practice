79523f03cf7602ac5574b85f950fc5f292d371a1
Aksi <137812264+AksiLipe@users.noreply.github.com>
2024-07-02 16:49:12 +0300

    add table Symbols in db, update templates

--- Practice/DuoCypher/models/Rating.py
+++ Practice/DuoCypher/models/Rating.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Rating(models.Model):
-    user = models.OneToOneField('User', on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     rating = models.IntegerField(default=0)
