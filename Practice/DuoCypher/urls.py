cfda80e84596757f94d8303b0ea300c7af70573f
Aksi <137812264+AksiLipe@users.noreply.github.com>
2024-07-06 15:02:16 +0300

    User authorization and registration has been completed

--- Practice/DuoCypher/urls.py
+++ Practice/DuoCypher/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('rating', views.rating, name='rating'),
-    path('profile', views.profile, name='profile'),
-    path('receiving', views.receiving, name='receiving'),
-    path('sending', views.sending, name='sending'),
+    path('', views.base, name='base'),
+    path('rating/', views.rating, name='rating'),
+    path('profile/', views.profile, name='profile'),
+    path('receiving/', views.receiving, name='receiving'),
+    path('sending/', views.sending, name='sending'),
     path('receiving/<int:level>/', views.receiving_level, name='receiving_level'),
     path('sending/<int:level>/', views.sending_level, name='sending_level'),
-    path('translator', views.translator, name='translator'),
+    path('translator/', views.translator, name='translator'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- Practice/DuoCypher/views.py
+++ Practice/DuoCypher/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from .models.Symbols import Symbols
+from django.contrib import messages
 
 
-def index(request):
-    return render(request, 'index.html')
+def base(request):
+    return render(request, 'base.html')
 
 
 def rating(request):
@@ -53,3 +57,32 @@ def receiving_level(request, level):
     }
     return render(request, 'receiving_level.html', context)
 
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(base)
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect(base)  # Перенаправьте на домашнюю страницу
+        else:
+            messages.error(request, "Ошибка авторизации. Проверьте введенные данные.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login_view.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(base)
