5d0f8a3a15fddb153748deb3f546c1db3e40f4b2
Maya010505 <mayushka0105@yandex.ru>
2024-07-10 11:57:01 +0300

    create translator.html, мне лень писать

--- Practice/DuoCypher/models/Symbols.py
+++ Practice/DuoCypher/models/Symbols.py
@@ -4,3 +4,6 @@ from django.db import models
 class Symbols(models.Model):
     symbol = models.CharField(max_length=1, blank=True)
     answer = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.symbol} - {self.answer}"
--- Practice/DuoCypher/views.py
+++ Practice/DuoCypher/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models.Symbols import Symbols
 
 
@@ -12,10 +13,6 @@ def rating(request):
 
 def profile(request):
     return render(request, 'profile.html')
-
-
-def translator(request):
-    return render(request, 'translator.html')
 
 
 def sending(request):
@@ -53,3 +50,32 @@ def receiving_level(request, level):
     }
     return render(request, 'receiving_level.html', context)
 
+
+def translator(request):
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text', '')
+        source_lang = request.POST.get('source_lang', 'en')
+        target_lang = request.POST.get('target_lang', 'morse')
+
+        translated_text = ''
+
+        if source_lang == 'en' and target_lang == 'morse':
+            for char in input_text.upper():
+                if Symbols.objects.filter(symbol=char).exists():
+                    translated_text += Symbols.objects.get(symbol=char).answer + ' '
+                else:
+                    translated_text += '?'
+        elif source_lang == 'morse' and target_lang == 'en':
+            morse_to_text = {s.answer: s.symbol for s in Symbols.objects.all()}
+            for code in input_text.split():
+                translated_text += morse_to_text.get(code, '?')
+
+        context = {
+            'input_text': input_text,
+            'translated_text': translated_text.strip(),
+            'source_lang': source_lang,
+            'target_lang': target_lang,
+        }
+        return render(request, 'translator.html', context)
+
+    return render(request, 'translator.html')
